c961753f7244e6fd78f5dbd436fefdeb8f10882b
oxygen <ooxyygen0@gmail.com>
2022-10-12 20:19:12 +0300

    create white triangle and circle classes

---
+++ src/Lab3/WhiteCircle.java
@@ -0,0 +1,9 @@
+package Lab3;
+
+public class WhiteCircle extends AbstractCircle{
+    public static final String COLOR = "WHITE";
+
+    public WhiteCircle(int centerX, int centerY, int radius) {
+        super(centerX, centerY, radius);
+    }
+}
