aad5537eed949c74a9273a028c47af2147c1c683
oxygen <ooxyygen0@gmail.com>
2022-10-12 21:47:30 +0300

    crate non-parameter constructors for circles and triangles

--- src/Lab3/BlackCircle.java
+++ src/Lab3/BlackCircle.java
@@ -3,6 +3,9 @@ package Lab3;
 public class BlackCircle extends AbstractCircle{
 
     public static final String COLOR= "BLACK";
+
+    public BlackCircle() {super();}
+
     public BlackCircle(int centerX, int centerY, int radius) {
         super(centerX, centerY, radius);
     }
